53306e486d2bb5e6cd08d4cc6e5121cc34bf69e6
sunsideaspect <serhii.davydov.contact@gmail.com>
2026-02-26 16:08:04 +0200

    Update mapdata.js

--- js/mapdata.js
+++ js/mapdata.js
@@ -1,79 +1,31 @@
 var simplemaps_countrymap_mapdata={
   main_settings: {
-    //General settings
-    width: "responsive", 
-    background_color: "#FFFFFF",
+    width: "responsive",
     background_transparent: "yes",
-    border_color: "#2a2a35",
-    
-    //State defaults
-    state_description: "",
-    state_color: "#1e1e26",
-    state_hover_color: "#2a2a35",
-    state_url: "",
-    border_size: 1.5,
-    all_states_inactive: "no",
     all_states_zoomable: "no",
+    state_color: "#1e1e26",
+    state_hover_color: "#252530",
+    border_color: "#2a2a35",
+    border_size: 1.2,
     
-    //Location defaults
-    location_description: "",
-    location_url: "",
-    location_color: "#ffcc00",
-    location_opacity: 0.9,
-    location_hover_opacity: 1,
-    location_size: 15,
+    // Налаштування точок міст
     location_type: "circle",
-    location_image_source: "frog.png",
-    location_border_color: "#000",
+    location_color: "#ffcc00",
+    location_size: 14,
+    location_opacity: 0.9,
     location_border: 2,
-    location_hover_border: 2.5,
-    all_locations_inactive: "no",
-    all_locations_hidden: "no",
+    location_border_color: "#020617",
     
-    //Label defaults
+    // Налаштування тексту на мапі
     label_color: "#ffffff",
-    label_hover_color: "#ffffff",
     label_size: 11,
     label_font: "Arial",
-    label_display: "auto",
-    label_scale: "yes",
-    hide_labels: "no",
-    hide_eastern_labels: "no",
-   
-    //Zoom settings
-    zoom: "no",
-    manual_zoom: "no",
-    back_image: "no",
-    initial_back: "no",
-    initial_zoom: "-1",
-    initial_zoom_solo: "no",
-    region_opacity: 1,
-    region_hover_opacity: 0.6,
-    zoom_out_incrementally: "yes",
-    zoom_percentage: 0.99,
-    zoom_time: 0.5,
+    label_display: "all",
     
-    //Popup settings
-    popup_nocss: "no",
-    popup_centered: "yes",
-    popup_lossy_eyeball: "no",
-    popup_outline: "no",
-    popup_shadow: "no",
-    popup_corners: "0",
-    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
-    popup_nocss: "no",
-    
-    //Advanced settings
+    // Advanced
     div: "map",
     auto_load: "yes",
-    url_new_tab: "no",
-    images_directory: "default",
-    fade_time: 0.1,
-    link_text: "View Website",
-    popups: "detect",
-    state_image_url: "",
-    state_image_position: "",
-    location_image_url: ""
+    zoom: "no"
   },
   state_specific: {
     UA01: { name: "Крим" }, UA05: { name: "Вінницька" }, UA07: { name: "Волинська" },
@@ -94,9 +46,7 @@ var simplemaps_countrymap_mapdata={
     "4": { name: "Дніпро", lat: "48.46", lng: "35.04" },
     "5": { name: "Полтава", lat: "49.58", lng: "34.55" },
     "6": { name: "Житомир", lat: "50.25", lng: "28.65" },
-    "7": { name: "Івано-Франківськ", lat: "48.92", lng: "24.71" }
-  },
-  labels: {
-    UA32: { parent_id: "UA32", x: "478", y: "175", text: "КИЇВ" }
+    "7": { name: "Хмельницький", lat: "49.42", lng: "26.98" },
+    "8": { name: "Васильків", lat: "50.17", lng: "30.31" }
   }
 };
